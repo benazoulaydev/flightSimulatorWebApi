1b25da23a8e4aa70e248b0f5037269b1bdc0b809
kfiryehuda <kfir.yehuda50@gmail.com>
2020-05-15 17:47:35 +0300

    start

---
+++ wwwroot/js/planeIcons.js
@@ -0,0 +1,17 @@
+﻿var map = L.map('map').invalidateSize();
+
+var blackIcon = L.icon({
+    iconUrl: 'lib/images/flight-icon-no-rotate.jpg',
+    iconSize: [38, 38], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-19, -38] // point from which the popup should open relative to the iconAnchor
+});
+L.marker([51.5, -0.09], { icon: blackIcon }).addTo(map);
+
+var LeafIcon = L.Icon.extend({
+    options: {
+        iconSize: [38, 95],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76]
+    }
+});
