ca4362ded9d6820e853ea657ad509f28980faa0a
kfiryehuda <kfir.yehuda50@gmail.com>
2020-05-22 14:09:47 +0300

    initial icons and line

--- wwwroot/js/planeIcons.js
+++ wwwroot/js/planeIcons.js
@@ -1,17 +1,43 @@
-﻿var map = L.map('map').invalidateSize();
+﻿//draws the plane icons
 
+var locations = [[32,32],[33.244, 40.12],[34,34]];
 var blackIcon = L.icon({
-    iconUrl: 'lib/images/flight-icon-no-rotate.jpg',
+    iconUrl: 'lib/images/flight-icon-no-rotate.png',
     iconSize: [38, 38], // size of the icon
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    popupAnchor: [-19, -38] // point from which the popup should open relative to the iconAnchor
+    iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -36] // point from which the popup should open relative to the iconAnchor
 });
-L.marker([51.5, -0.09], { icon: blackIcon }).addTo(map);
+var selectedIcon = L.icon({
+    iconUrl: 'lib/images/flight-icon-no-rotate-select.png',
+    iconSize: [38, 38], // size of the icon
+    iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -36] // point from which the popup should open relative to the iconAnchor
+});
+// for(let location of locations){
+//     L.marker(location, { icon: blackIcon }).addTo(map);
+// }
+var selected;
+for (i = 0; i < locations.length; i++) {
+    let testmarker = [];
+    console.log([locations[i][0],locations[i][1]]);
+    testmarker[i] = L.marker([locations[i][0],locations[i][1]], {icon: blackIcon, id: i});
+    testmarker[i].on('click', function(e) {
+        if (typeof selected !== 'undefined') {
+            selected.target.setIcon(blackIcon);
+        }
+        selected = e;
+        e.target.setIcon(selectedIcon);
+        //document.getElementById('someDiv').innerHTML = points[e.target.options.id][2];
+    });
+    map.addLayer(testmarker[i]);
+}
 
-var LeafIcon = L.Icon.extend({
-    options: {
-        iconSize: [38, 95],
-        iconAnchor: [22, 94],
-        popupAnchor: [-3, -76]
-    }
-});
+// create a red polyline from an array of LatLng points
+var latlngs = [
+    [45.51, -122.68],
+    [37.77, -122.43],
+    [34.04, -118.2]
+];
+var polyline = L.polyline(locations, {color: 'red'}).addTo(map);
+// zoom the map to the polyline
+map.fitBounds(polyline.getBounds());
