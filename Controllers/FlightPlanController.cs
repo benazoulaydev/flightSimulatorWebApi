90ae946ec4bb1190a697a5ae9771d28181bc0a71
Hagai Stavi <hagai.stavi@live.biu.ac.il>
2020-06-01 17:34:30 +0300

    ok result actionts

--- Controllers/FlightPlanController.cs
+++ Controllers/FlightPlanController.cs
@@ -80,7 +80,7 @@ namespace FlightSimulatorWebApi.Controllers
                 flightPlanID = GetFlightID();
             } while (flightPlans.ContainsKey(flightPlanID));
             flightPlans.Add(flightPlanID, info);
-            return Ok(info);
+            return info;
         }
 
         /// <summary>
@@ -120,9 +120,8 @@ namespace FlightSimulatorWebApi.Controllers
                 {
                     return NotFound();
                 }
-
             }
-            return Ok(flightPlan); ;
+            return flightPlan;
         }
 
         /// <summary>
@@ -143,10 +142,7 @@ namespace FlightSimulatorWebApi.Controllers
 
                     FlightPlan serverflightPlan = JsonConvert.DeserializeObject<FlightPlan>(resp);
 
-                    return Ok(serverflightPlan);
-
-
-
+                    return serverflightPlan;
                 }
             }
             else
@@ -232,7 +228,7 @@ namespace FlightSimulatorWebApi.Controllers
             // check if there any flight at all
             if (!cache.TryGetValue("FlightPlans", out flightPlans))
             {
-                return Ok(flightList); // return empty list
+                return flightList; // return empty list
             }
 
             // going throw each flight
@@ -244,7 +240,7 @@ namespace FlightSimulatorWebApi.Controllers
             Dictionary<string, Servers> servers;
             if (!Request.QueryString.Value.Contains("sync_all") || !cache.TryGetValue("servers", out servers))
             {
-                return Ok(flightList);
+                return flightList;
             }
             foreach (KeyValuePair<string, Servers> server in servers)
             {
@@ -264,7 +260,7 @@ namespace FlightSimulatorWebApi.Controllers
                     break;
                 }
             }
-            return Ok(flightList);
+            return flightList;
         }
 
 
@@ -275,7 +271,7 @@ namespace FlightSimulatorWebApi.Controllers
         /// <returns></returns>
         [HttpDelete]
         [Route("Flights/{id}")]
-        public ActionResult<FlightPlan> DeleteFlight(string id)
+        public ActionResult<string> DeleteFlight(string id)
         {
             Dictionary<string, FlightPlan> flightPlans;
             if (!cache.TryGetValue("FlightPlans", out flightPlans))
@@ -288,7 +284,7 @@ namespace FlightSimulatorWebApi.Controllers
                 return NotFound();
             }
             flightPlans.Remove(id);
-            return Ok(id);
+            return id;
         }
 
         /// <summary>
