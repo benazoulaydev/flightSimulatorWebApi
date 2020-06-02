def3a39b013d31dccfc9287e522a355f7c7108cb
kfiryehuda <kfir.yehuda50@gmail.com>
2020-06-02 10:53:44 +0300

    fix 2 json

---
+++ out/wwwroot/js/ajaxFlight.js
@@ -0,0 +1,87 @@
+﻿
+//call function 4 time per sec (every 250 milisec)
+
+(function loop() {
+    get_flight_time();
+    setTimeout(loop, 1000); //recurse
+})();
+function get_flight_time() {
+
+    var today = new Date();
+    if ((today.getMonth() + 1) < 10) { var mounth = "0" + (today.getMonth() + 1); } else { var mounth = (today.getMonth() + 1); }
+    if (today.getDate() < 10) { var day = "0" + today.getDate(); } else { var day = today.getDate(); }
+    if (today.getHours() < 10) { var hour = "0" + today.getHours(); } else { var hour = today.getHours(); }
+    if (today.getMinutes() < 10) { var min = "0" + today.getMinutes(); } else { var min = today.getMinutes(); }
+    if (today.getSeconds() < 10) { var sec = "0" + today.getSeconds(); } else { var sec = today.getSeconds(); }
+    var time = today.getFullYear() + "-" + mounth + "-" + day + "T" + hour + ":" + min + ":" + sec;
+
+
+    $.ajax({
+        type: "GET",
+        url: "/api/Flights?relative_to=" + time + "&sync_all", // Using our resources.json file to serve results
+        dataType: 'json',
+        success: function (result) {
+            display_flights(result);
+            display_planes_icons(result);
+            // call function with parameter result
+			console.log(result);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+
+        }
+    });
+}
+
+function get_flightplan(id) {
+    $.ajax({
+        type: "GET",
+        url: "/api/FlightPlan/" + id, // Using our resources.json file to serve results
+        dataType: 'json',
+        success: function (result) {
+            // call function with parameter result
+            setFlightPlanBox(result, id);
+            drawFlightPlanLine(result, id);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            let al = document.getElementById('alrt');
+            al.innerHTML = '<b>Error </b>' + XMLHttpRequest.responseJSON.status + ", " + XMLHttpRequest.responseJSON.title;
+            al.style.opacity = "1.0";
+            setTimeout(function () { document.getElementById('alrt').style.opacity = "0.0"; }, 5000);
+        }
+    });
+}
+
+function post_flightplan(flightPlan) {
+    $.ajax({
+        type: "POST",
+        url: "/api/FlightPlan", // Using our resources.json file to serve results
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: flightPlan,
+        success: function (result) {
+            // call function with parameter result
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            let al = document.getElementById('alrt');
+            al.innerHTML = '<b>Error </b>' + XMLHttpRequest.responseJSON.status + ", " + XMLHttpRequest.responseJSON.title;
+            al.style.opacity = "1.0";
+            setTimeout(function () { document.getElementById('alrt').style.opacity = "0.0"; }, 5000);
+        }
+    });
+}
+
+function delete_flightplan(id) {
+    $.ajax({
+        type: "DELETE",
+        url: "/api/Flights/" + id, // Using our resources.json file to serve results
+        success: function (result) {
+            // call function with no parameter
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            let al = document.getElementById('alrt');
+            al.innerHTML = '<b>Error </b>' + XMLHttpRequest.responseJSON.status + ", " + XMLHttpRequest.responseJSON.title;
+            al.style.opacity = "1.0";
+            setTimeout(function () { document.getElementById('alrt').style.opacity = "0.0"; }, 5000);
+        }
+    });
+}
---
+++ out/wwwroot/js/flights_list.js
@@ -0,0 +1,109 @@
+﻿var chosenID = null;
+var prevDeleted = null;
+
+function display_flights(flights) {
+    tableHtml = "";
+    document.getElementById("internal_flights").innerHTML = tableHtml;
+    for (i = 0; i < flights.length; i++) {
+        if (!flights[i].is_external) {
+            var long = flights[i].longitude.toString().substring(0, 5);
+            var lati = flights[i].latitude.toString().substring(0, 5);
+            tableHtml += "<a id='" + flights[i].flight_id + "' href='#' onclick='chooseLine(this.id)' class='list-group-item list-group-item-action flex-column align-items-start'>" +
+                "<div class='d-flex w-100 justify-content-between' >" +
+                "<h5 class='mb-1'>" + flights[i].flight_id + "   " + flights[i].company_name + "</h5>" +
+                "</div >" +
+                "<small>(" + long + ", " + lati + ")</small>" +
+                "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                "</a >" +
+                "<label id='" + flights[i].flight_id + "' onclick='deleteLine(this.id)'>X</label>";
+        }
+    }
+    tableHtml += "<div class='d-flex w-100 justify-content-between'><h5 class='mb-0 text-center'>Externals</h5></div>";
+
+    for (i = 0; i < flights.length; i++) {
+        if (flights[i].is_external) {
+            var long = flights[i].longitude.toString().substring(0, 5);
+            var lati = flights[i].latitude.toString().substring(0, 5);
+            tableHtml += "<a id='" + flights[i].flight_id + "' href='#' onclick='chooseLine(this.id)' class='list-group-item list-group-item-action flex-column align-items-start'>" +
+                "<div class='d-flex w-100 justify-content-between' >" +
+                "<h5 class='mb-1'>" + flights[i].flight_id + "   " + flights[i].company_name + "</h5>" +
+                "</div >" +
+                "<small>(" + long + ", " + lati + ")</small>" +
+                "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                "</a >";
+        }
+    }
+    document.getElementById("internal_flights").innerHTML = tableHtml;
+    if (chosenID !== null) {
+        console.log("kfir2" + chosenID);
+        document.getElementById(chosenID).style.backgroundColor = "powderblue";
+    }
+}
+
+function upload(ev) {
+    var element = document.getElementById("upLoad");
+    var jsonfile;
+    if (element.files.length > 0) {
+        jsonfile = element.files[0];
+    }
+
+    var text;
+
+    var reader = new FileReader();
+    reader.onload = (function (data) {
+        return function (e) {
+            text = JSON.parse(e.target.result);
+            text = JSON.stringify(text);
+            post_flightplan(text);
+        }
+    })(jsonfile);
+
+    reader.readAsText(jsonfile);
+}
+
+function chooseLine(id) {
+    if (chosenID !== null) {
+        document.getElementById(chosenID).style.backgroundColor = "powderblue";
+    }
+    chosenID = id;
+    get_flightplan(id);
+}
+
+function deleteLine(id) {
+    if (prevDeleted === id) {
+        return;
+    }
+    prevDeleted = id;
+    if (id === chosenID) {
+        chosenID = null;
+    }
+    delete_flightplan(id);
+    delete_flight(id);
+
+    document.getElementById("identifer").innerHTML = id + " has deleted";
+    document.getElementById("startPosition").innerHTML = "";
+    document.getElementById("endPosition").innerHTML = "";
+}
+
+function resetBox() {
+    document.getElementById("identifer").innerHTML = "";
+    document.getElementById("startPosition").innerHTML = "";
+    document.getElementById("endPosition").innerHTML = "";
+}
+
+function setFlightPlanBox(flightPlan, id) {
+    var long = flightPlan.initial_location.longitude.toString().substring(0, 5);
+    var lati = flightPlan.initial_location.latitude.toString().substring(0, 5);
+
+    document.getElementById("identifer").innerHTML = id + " for " + flightPlan.company_name;
+    document.getElementById("startPosition").innerHTML = "Begining at " + flightPlan.initial_location.date_time + " in (" +
+        long + ", " + lati + ")";
+    var endTime = new Date(flightPlan.initial_location.date_time);
+    for (i = 0; i < flightPlan.segments.length; i++) {
+        endTime.setSeconds(endTime.getSeconds() + flightPlan.segments[i].timespan_seconds);
+    }
+    var endLongitude = flightPlan.segments[flightPlan.segments.length - 1].longitude.toString().substring(0, 5);
+    var endLatitude = flightPlan.segments[flightPlan.segments.length - 1].latitude.toString().substring(0, 5);
+    var endTimeFormat = endTime.getFullYear() + "-" + (endTime.getMonth() + 1) + "-" + endTime.getDate() + "T" + endTime.getHours() + ":" + endTime.getMinutes() + ":" + endTime.getSeconds();
+    document.getElementById("endPosition").innerHTML = "Ending at " + endTimeFormat + " in (" + endLongitude + ", " + endLatitude + ")";
+}
